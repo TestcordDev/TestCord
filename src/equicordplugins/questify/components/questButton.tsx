e9853083850dfcb4bb9fce3a6868c2a18bdd9d37
x2b <mokihbbjjj@gmail.com>
2026-07-21 19:52:45 +0200

    fixes regarding new canary errors

--- src/equicordplugins/questify/components/questButton.tsx
+++ src/equicordplugins/questify/components/questButton.tsx
@@ -19,7 +19,7 @@ import { decimalToRGB, formatLowerBadge, isDarkish, leftClick, middleClick, q, Q
 import { openQuestifySettingsModal } from "./settingsModal";
 
 const GuildlessServerListItemComponent = findComponentByCodeLazy("tooltip:", "lowerBadgeSize:");
-const ServerListItemPillComponent = findComponentByCodeLazy('"pill":"empty"');
+const ServerListItemPillComponent = findComponentByCodeLazy("overlay:d=!1", "unread:r=!1");
 const ServerListItemLowerBadgeComponent = findComponentByCodeLazy("BADGE_NOTIFICATION_BACKGROUND", "let{count:");
 
 interface QuestButtonLowerBadgeProps {
