19616cafdbab232f08b0a1e5d0e7d1517e46a487
DxRx99 <170854302+DxRx99@users.noreply.github.com>
2026-07-30 21:22:17 +0300

    wip: game activity toggle background pill styling (unfinished)

--- src/plugins/gameActivityToggle/index.tsx
+++ src/plugins/gameActivityToggle/index.tsx
@@ -18,13 +18,13 @@
 
 import { isPluginEnabled } from "@api/PluginManager";
 import { definePluginSettings } from "@api/Settings";
-import { UserAreaRenderProps } from "@api/UserArea";
+import { UserAreaButton, UserAreaRenderProps } from "@api/UserArea";
 import { getUserSettingLazy } from "@api/UserSettings";
 import testcordToolbox from "@testcordplugins/testcordToolbox";
 import { Devs, TestcordDevs } from "@utils/constants";
 import definePlugin, { OptionType } from "@utils/types";
 import { FluxStore } from "@vencord/discord-types";
-import { findByPropsLazy, findComponentByCodeLazy, findStoreLazy } from "@webpack";
+import { findByPropsLazy, findStoreLazy } from "@webpack";
 import { Menu, Popout, useRef, useState, useStateFromStores } from "@webpack/common";
 interface ConnectedAccount {
     id: string;
@@ -37,7 +37,6 @@ interface ConnectedAccountsStore extends FluxStore {
     getAccounts(): ConnectedAccount[];
 }
 
-const Button = findComponentByCodeLazy(".GREEN,positionKeyStemOverride:");
 const ConnectedAccountsStore = findStoreLazy("ConnectedAccountsStore") as ConnectedAccountsStore;
 const ConnectedAccountActions = findByPropsLazy("setShowActivity");
 
@@ -48,6 +47,11 @@ const settings = definePluginSettings({
         type: OptionType.BOOLEAN,
         description: "Use the old icon style before Discord icon redesign",
         default: false
+    },
+    sameColorAsOtherButtons: {
+        type: OptionType.BOOLEAN,
+        description: "Match the color of other panel buttons in active state",
+        default: true
     },
     forcedColor: {
         type: OptionType.BOOLEAN,
@@ -68,7 +72,7 @@ const settings = definePluginSettings({
 });
 
 function Icon({ className }: { className?: string; }) {
-    const { oldIcon, forcedColor } = settings.use(["oldIcon", "forcedColor"]);
+    const { oldIcon, forcedColor, sameColorAsOtherButtons } = settings.use(["oldIcon", "forcedColor", "sameColorAsOtherButtons"]);
     const showCurrentGame = ShowCurrentGame.useSetting();
 
     const redLinePath = !oldIcon
@@ -79,9 +83,15 @@ function Icon({ className }: { className?: string; }) {
         ? "M23.27 4.73 19.27 .73 -.27 20.27 3.73 24.27Z"
         : "M23.27 4.54 19.46.73 .73 19.46 4.54 23.27 23.27 4.54Z";
 
-    const mainFill = forcedColor
-        ? (showCurrentGame ? "var(--interactive-normal, currentColor)" : "var(--status-danger, #da373c)")
-        : (!showCurrentGame && !oldIcon ? "var(--status-danger, #da373c)" : "currentColor");
+    const mainFill = sameColorAsOtherButtons
+        ? "currentColor"
+        : (forcedColor
+            ? (showCurrentGame ? "var(--interactive-normal, currentColor)" : "var(--status-danger, #da373c)")
+            : (!showCurrentGame && !oldIcon ? "var(--status-danger, #da373c)" : "currentColor"));
+
+    const lineFill = sameColorAsOtherButtons
+        ? "currentColor"
+        : "var(--status-danger, #da373c)";
 
     return (
         <svg className={className} width="20" height="20" viewBox="0 0 24 24" style={{ color: mainFill }}>
@@ -91,7 +101,7 @@ function Icon({ className }: { className?: string; }) {
                 d="M3.06 20.4q-1.53 0-2.37-1.065T.06 16.74l1.26-9q.27-1.8 1.605-2.97T6.06 3.6h11.88q1.8 0 3.135 1.17t1.605 2.97l1.26 9q.21 1.53-.63 2.595T20.94 20.4q-.63 0-1.17-.225T18.78 19.5l-2.7-2.7H7.92l-2.7 2.7q-.45.45-.99.675t-1.17.225Zm14.94-7.2q.51 0 .855-.345T19.2 12q0-.51-.345-.855T18 10.8q-.51 0-.855.345T16.8 12q0 .51.345 .855T18 13.2Zm-2.4-3.6q.51 0 .855-.345T16.8 8.4q0-.51-.345-.855T15.6 7.2q-.51 0-.855.345T14.4 8.4q0 .51.345 .855T15.6 9.6ZM6.9 13.2h1.8v-2.1h2.1v-1.8h-2.1v-2.1h-1.8v2.1h-2.1v1.8h2.1v2.1Z"
             />
             {!showCurrentGame && <>
-                <path fill="var(--status-danger, #da373c)" d={redLinePath} />
+                <path fill={lineFill} d={redLinePath} />
                 <mask id="gameActivityMask">
                     <rect fill="white" x="0" y="0" width="24" height="24" />
                     <path fill="black" d={maskBlackPath} />
@@ -101,8 +111,8 @@ function Icon({ className }: { className?: string; }) {
     );
 }
 
-function GameActivityToggleButton(props: UserAreaRenderProps) {
-    const { location } = settings.use(["location"]);
+function GameActivityToggleButton({ iconForeground, hideTooltips, nameplate }: UserAreaRenderProps) {
+    const { location, sameColorAsOtherButtons } = settings.use(["location", "sameColorAsOtherButtons"]);
     const showCurrentGame = ShowCurrentGame.useSetting();
 
     const connectedAccounts = useStateFromStores([ConnectedAccountsStore], () => ConnectedAccountsStore.getAccounts());
@@ -115,18 +125,18 @@ function GameActivityToggleButton(props: UserAreaRenderProps) {
     if (location !== "PANEL" && isPluginEnabled(testcordToolbox.name)) return null;
 
     const buttonProps = {
-        tooltipText: showCurrentGame ? "Disable Game Activity" : "Enable Game Activity",
-        icon: Icon,
+        tooltipText: hideTooltips ? void 0 : (showCurrentGame ? "Disable Game Activity" : "Enable Game Activity"),
+        icon: <Icon className={iconForeground} />,
         role: "switch",
-        ariaChecked: !showCurrentGame,
-        redGlow: !showCurrentGame,
-        plated: props?.nameplate != null,
+        "aria-checked": !showCurrentGame,
+        redGlow: sameColorAsOtherButtons ? false : !showCurrentGame,
+        plated: nameplate != null,
         onClick: () => ShowCurrentGame.updateSetting(old => !old)
     };
 
     // Only show switch if there's exactly one Spotify account connected. Otherwise it may lead to confusion
     if (spotifyAccounts.length !== 1)
-        return <Button {...buttonProps} />;
+        return <UserAreaButton {...buttonProps} />;
 
     const spotifyAccount = spotifyAccounts[0];
 
@@ -150,7 +160,7 @@ function GameActivityToggleButton(props: UserAreaRenderProps) {
             )}
         >
             {popoutProps => (
-                <Button
+                <UserAreaButton
                     ref={buttonRef}
                     onContextMenu={popoutProps.onClick}
                     {...buttonProps}
