d01739ff40b54fa0b3c44d0c9fe625e733c6e43e
x2b <mokihbbjjj@gmail.com>
2026-08-19 02:15:59 +0200

    vro

--- who-fucking-cares-dude/src/testcordplugins/bypassBlockedOrIgnored/stores.js
+++
@@ -1,7 +0,0 @@
-/*
- * Vencord, a Discord client mod
- * Copyright (c) 2025 Vendicated and contributors
- * SPDX-License-Identifier: GPL-3.0-or-later
- */
-import { findStoreLazy } from "@webpack";
-export const RelationshipStore = findStoreLazy("RelationshipStore");
